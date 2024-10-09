375370b11ee9b180e8be3101995a9cb61ee83d22
jerreramarcia <jerreramarcia@gmail.com>
2024-10-09 18:30:04 +0000

    Pages folder created and tailwind config

---
+++ pages/hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>Hello World!</h1>
+    <h2 class="text-3xl underline">
+    Hello world!2
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<style scoped>
+.hello {
+  font-family: Arial, sans-serif;
+}
+</style>
